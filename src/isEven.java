b92f43fde6299ec0e8a8c40f7726d5cc02e1383a
Mohammad Nabelsi <m7.nabelsi@gmail.com>
2024-03-12 09:47:53 +0100

    UPD Method is Even

--- src/isEven.java
+++ src/isEven.java
@@ -2,11 +2,12 @@ import java.util.Scanner;
 
 public class isEven {
     public static void main(String[] args) {
-
         getEven();
-
     }
 
+    /**
+     * method is even found number input from user
+     */
     public static void getEven() {
 
         Scanner scanner = new Scanner(System.in);
