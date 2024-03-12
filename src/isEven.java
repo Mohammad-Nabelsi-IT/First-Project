b426d92e6f9740bb8298737632147feeb1c9ed0a
Mohammad Nabelsi <m7.nabelsi@gmail.com>
2024-03-12 09:31:38 +0100

    ADD Method is Even

---
+++ src/isEven.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class isEven {
+    public static void main(String[] args) {
+
+        getEven();
+
+    }
+
+    public static void getEven() {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("Please Enter Number : ");
+        int number = scanner.nextInt();
+        while (true) {
+            if (number % 2 == 0) {
+                System.out.printf("is Even Number %d %n", number);
+
+                // again Question
+                System.out.printf("Please Enter New Number?");
+                number = scanner.nextInt();
+            } else {
+                System.out.printf("is Odd Number %d %n",number);
+                break;
+            }
+
+
+        }
+
+    }
+}
